44bff333adcc965a21fab59ceaf9b3773f70ad6e
CodyMurray-ga <codakmurraki@gmail.com>
2022-05-16 16:46:09 -0500

    start skill list to be rendered in main doc

---
+++ js/project-list.js
@@ -0,0 +1,8 @@
+const projects = [
+  {
+    name: 'Six-String Theory',
+    technologies: ['React', 'HTML5', 'CSS3'],
+    'source code': 'https://github.com/CodyMurr/guitar-theory',
+    deployment: 'https://scalenotetheory.netlify.app/',
+  },
+];
