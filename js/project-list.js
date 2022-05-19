20b529651bb9eeedb7e3d6711277b6216f5f7a46
CodyMurr <codakmurraki@gmail.com>
2022-05-18 20:03:30 -0500

    render projects

--- js/project-list.js
+++
@@ -1,8 +0,0 @@
-const projects = [
-  {
-    name: 'Six-String Theory',
-    technologies: ['React', 'HTML5', 'CSS3'],
-    'source code': 'https://github.com/CodyMurr/guitar-theory',
-    deployment: 'https://scalenotetheory.netlify.app/',
-  },
-];
